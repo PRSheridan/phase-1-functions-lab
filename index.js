6ec74805e9ebdcd11efc3a7a0f0826ab511ffe68
Phil <philrsheridan@gmail.com>
2023-09-20 20:57:26 -0400

    passed 13 tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(loc){
+    if(loc >= 42){
+        return loc % 42;
+    }else if(loc < 42){
+        return 42 - loc;
+    }
+}
+
+function distanceFromHqInBlocksAlt(loc, hq){
+    if(loc >= hq){
+        return loc % hq;
+    }else if(loc < hq){
+        return hq - loc;
+    }
+}
+
+function distanceFromHqInFeet(loc){
+    return distanceFromHqInBlocks(loc)*264;
+}
+
+function distanceTravelledInFeet(loc, hq){
+    return distanceFromHqInBlocksAlt(loc, hq)*264;
+}
+
+function calculatesFarePrice(loc, hq){
+    let distTravelled = distanceTravelledInFeet(loc, hq)
+
+    if(distTravelled <= 400){
+        return 0;
+    }else if(distTravelled > 400 && distTravelled <= 2000){
+        return ((distTravelled-400)*2)/100;
+    }else if(distTravelled > 2000 && distTravelled <= 2500){
+        return 25;
+    } else if(distTravelled > 2500){
+        return "cannot travel that far";
+    }
+}
